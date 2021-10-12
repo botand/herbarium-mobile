a3a3e6319a9ee1dae288b2b8c3d6dfe136764fb3
apomalyn <chretienxavier42@gmail.com>
2021-10-12 14:53:01 -0400

    Another test

--- lib/src/ui/home/home_view.dart
+++ lib/src/ui/home/home_view.dart
@@ -4,8 +4,6 @@ import 'package:herbarium_mobile/src/ui/base/base_scaffold.dart';
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) =>
       const BaseScaffold(body: Center(child: Text("Home")));
