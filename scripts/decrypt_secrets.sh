7046600b91ba07634e7774c5cb41ded7ac716079
Xavier Chrétien <chretienxavier42@gmail.com>
2021-12-02 02:09:57 -0500

    Remove test line

--- scripts/decrypt_secrets.sh
+++ scripts/decrypt_secrets.sh
@@ -10,5 +10,3 @@ if [[ -n "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" ]]; then
   openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./encryptedFiles/google-services.json.enc -out ./android/app/google-services.json -md md5
   openssl enc -aes-256-cbc -pbkdf2 -d -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -in ./encryptedFiles/GoogleService-Info.plist.enc -out ./ios/GoogleService-Info.plist -md md5
 fi
-
-openssl enc -aes-256-cbc -pbkdf2 -k "$ENCRYPTED_GOOGLE_SERVICE_PASSWORD" -out ./encryptedFiles/google-services.json.enc -in ./android/app/google-services.json -md md5
