d987b1177a063ab237e3ce7cc0e703f7b019e840
apomalyn <chretienxavier42@gmail.com>
2021-12-06 21:21:51 -0500

    Implement Plant button used to represent a slot of a greenhouse (with/without plant, with light on/off)

---
+++ lib/src/ui/base/plant_pot_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:herbarium_mobile/src/core/models/plant.dart';
+
+class PlantPotButton extends StatelessWidget {
+  final VoidCallback? onTap;
+
+  final Plant? plant;
+
+  final bool showLabel;
+
+  const PlantPotButton({Key? key, this.onTap, this.plant, this.showLabel = true})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => InkWell(
+    onTap: onTap,
+    child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: plant != null ? _buildPlant() : _buildFrenchClip()),
+  );
+
+  List<Widget> _buildPlant() {
+    String imageAsset = "assets/images/plant_pot_with_light_off.png";
+
+    if (plant!.ligthStripStatus != null && plant!.ligthStripStatus!.status) {
+      imageAsset = "assets/images/plant_pot_with_light_on.png";
+    }
+    return [
+      Image.asset(imageAsset),
+      if (showLabel)
+        Padding(
+          padding: const EdgeInsets.only(top: 0.0),
+          child: Center(child: Text(plant!.type.name)),
+        )
+    ];
+  }
+
+  List<Widget> _buildFrenchClip() =>
+      [Image.asset("assets/images/french_clip_light_off.png")];
+}
