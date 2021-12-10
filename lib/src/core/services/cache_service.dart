d478224a4582ecbabfac26c6748a5e0ec515e21d
apomalyn <chretienxavier42@gmail.com>
2021-12-09 23:40:11 -0500

    Implement cache service.

---
+++ lib/src/core/services/cache_service.dart
@@ -0,0 +1,75 @@
+import 'dart:convert';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart' as lib;
+import 'package:herbarium_mobile/src/core/locator.dart';
+import 'package:herbarium_mobile/src/core/services/analytics_service.dart';
+import 'package:herbarium_mobile/src/core/utils/cache_exception.dart';
+
+/// Abstraction of the cache management system.
+class CacheService {
+  /// Name of the cache.
+  static const _key = 'HerbariumCache';
+
+  final lib.CacheManager _cacheManager = lib.CacheManager(
+    lib.Config(_key,
+        stalePeriod: const Duration(days: 30),
+        maxNrOfCacheObjects: 20,
+        repo: lib.CacheObjectProvider(databaseName: _key)),
+  );
+
+  final AnalyticsService _analyticsService = locator<AnalyticsService>();
+
+  /// Get from the cache the value associated with [key].
+  /// Throw a [CacheException] if the [key] doesn't exist.
+  Future<String> get(String key) async {
+    final lib.FileInfo? fileInfo = await _cacheManager.getFileFromCache(key);
+
+    if (fileInfo == null) {
+      _analyticsService.logEvent(runtimeType.toString(),
+          "Trying to access $key from the cache but file doesn't exists");
+      throw CacheException(
+          prefix: runtimeType.toString(),
+          message: "$key doesn't exist in the cache");
+    }
+
+    return fileInfo.file.readAsString();
+  }
+
+  /// Update/create in the cache the value associated with [key].
+  Future update(String key, String value) async {
+    try {
+      await _cacheManager.putFile(
+          key, UriData.fromString(value, encoding: utf8).contentAsBytes());
+    } on Exception catch (e, stacktrace) {
+      _analyticsService.logError(
+          runtimeType.toString(),
+          "Exception raised during cache update of $key: ${e.toString()}",
+          e,
+          stacktrace);
+      rethrow;
+    }
+  }
+
+  /// Delete from the cache the content associated with [key].
+  Future delete(String key) async {
+    try {
+      await _cacheManager.removeFile(key);
+    } on Exception catch (e, stacktrace) {
+      _analyticsService.logError(
+          runtimeType.toString(),
+          "Exception raised during cache delete of $key: ${e.toString()}",
+          e,
+          stacktrace);
+    }
+  }
+
+  /// Empty the cache
+  Future empty() async {
+    try {
+      await _cacheManager.emptyCache();
+    } on Exception catch (e, stacktrace) {
+      _analyticsService.logError(runtimeType.toString(),
+          "Exception raised during emptying cache: $e", e, stacktrace);
+      rethrow;
+    }
+  }
+}
